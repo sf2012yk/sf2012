99c7ca62c69eb7c6e06473b0902c7c62ea13856d
sf2012yk <sf2012yk@gmail.com>
2025-11-09 09:42:55 +0100

    Add files via upload

---
+++ js/map/dubai.js
@@ -0,0 +1,13 @@
+
+var map = L.map('mapdubai', {center: [25.24916816381139, 55.361520925952654],
+        zoom: 13.4,
+        zoomControl: false,
+        scrollWheelZoom: false,  // ホイールズーム禁止
+        doubleClickZoom: false,  // ダブルクリックズーム禁止
+        boxZoom: false,          // ボックスズーム禁止
+        keyboard: false          // キーボード操作禁止
+        });
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© <a href="http://osm.org/copyright">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">Dubai UAE</a>'
+    }).addTo(map);
