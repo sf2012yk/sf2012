56a0d7dbe2ca0ef52f601cc578984fa8aae73bc5
sf2012yk <sf2012yk@gmail.com>
2025-11-16 14:45:30 +0100

    Add files via upload

--- js/icons/airport/airport.js
+++ js/icons/airport/airport.js
@@ -165,13 +165,13 @@ var markerViennaAirport = L.marker([48.11851237806887, 16.566266743122807], {
 //  Dubai
     var popupContent = `
       <div class="popup_summer">
-    <a href="https://lh3.googleusercontent.com/pw/AP1GczMuyA6PtvVMNcsHFq5pBmUGW9BLRnIzrcVHprpIvRTyZDK3v28W9eCC4bdIGJ0Xh40CIce1YP4BoGizd8PCq5Ch5Vs4ETUuLwJcz-sPSebc8w6rMI_HhGCXdhq38qQWKDjafA11HVhSJcvLLGUkwg7xsg=w1216-h913-s-no-gm?authuser=0" 
+    <a href="Dubai.html" 
        data-lightbox="image" 
        data-title="Dubai">
       <img class="popup_img" loading="eager" src="https://lh3.googleusercontent.com/pw/AP1GczMuyA6PtvVMNcsHFq5pBmUGW9BLRnIzrcVHprpIvRTyZDK3v28W9eCC4bdIGJ0Xh40CIce1YP4BoGizd8PCq5Ch5Vs4ETUuLwJcz-sPSebc8w6rMI_HhGCXdhq38qQWKDjafA11HVhSJcvLLGUkwg7xsg=w1216-h913-s-no-gm?authuser=0" alt="Dubai Airport">
     </a>
     <div class="popup_caption">
-    <a href="https://lh3.googleusercontent.com/pw/AP1GczMuyA6PtvVMNcsHFq5pBmUGW9BLRnIzrcVHprpIvRTyZDK3v28W9eCC4bdIGJ0Xh40CIce1YP4BoGizd8PCq5Ch5Vs4ETUuLwJcz-sPSebc8w6rMI_HhGCXdhq38qQWKDjafA11HVhSJcvLLGUkwg7xsg=w1216-h913-s-no-gm?authuser=0" data-lightbox="image" data-title="Dubai Airport" class="popup_link">Dubai Airport</a>
+    <a href="Dubai.html" class="popup_link">Dubai Airport</a>
     <p>
     Dubaiの説明
     </p>
