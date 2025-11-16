d16923cd42ffd9bbd6b4f1f9e5a69be437d5fb32
sf2012yk <sf2012yk@gmail.com>
2025-11-16 14:24:19 +0100

    Add files via upload

--- js/icons/airport/airport.js
+++ js/icons/airport/airport.js
@@ -22,16 +22,19 @@ className: "icon-airport"
     <div class="popup_caption">
     <a href="Heathrow.html" class="popup_link">Heathrow Terminal3</a>
     <p>
-    ヒースロー空港第3ターミナルの説明
+    ヒースロー空港第3ターミナル。ここからは主に長距離国際線が発着。
     </p>
     </div>
     <a onclick="toggleSection('nextVisitSection')" class="popup_link">▼ Next Airport</a>
     <div id="nextVisitSection" class="popup_section">
-    <div class="next_card" id="HeathrowToDubaiCard">
-    <p class="next_text">&#x2708; Dubai（5000km）</span>
-    </div>
     <div class="next_card" id="HeathrowToViennaCard">
     <p class="next_text">&#x2708; Vienna（1200km）</span>
+    </div>
+    <div class="next_card" id="HeathrowToRomeCard">
+    <p class="next_text">&#x2708; Rome（1400km）</span>
+    </div>
+    <div class="next_card" id="HeathrowToDubaiCard">
+    <p class="next_text">&#x2708; Dubai（5000km）</span>
     </div>
     <div class="next_card" id="HeathrowTerminal3ToPaddingtonCard">
     <p class="next_text">London City（23km）</span>
@@ -64,6 +67,12 @@ var markerHeathrowTerminal3 = L.marker([51.46896718671588, -0.4574958618544679],
     </div>
     <a onclick="toggleSection('nextVisitSection')" class="popup_link">▼ Next Airport</a>
     <div id="nextVisitSection" class="popup_section">
+    <div class="next_card" id="HeathrowToParisCard">
+    <p class="next_text">&#x2708; Paris（400km）</span>
+    </div>
+    <div class="next_card" id="HeathrowToAmsterdamCard">
+    <p class="next_text">&#x2708; Amsterdam（400km）</span>
+    </div>
     <div class="next_card" id="HeathrowToGenevaCard">
     <p class="next_text">&#x2708; Geneva（600km）</span>
     </div>
@@ -144,6 +153,44 @@ var markerGenevaAirport = L.marker([46.2314339144919, 6.10746786781946], {
   </div>
 `;
 var markerViennaAirport = L.marker([48.11851237806887, 16.566266743122807], {
+  icon: AirportIcon 
+})
+  .bindPopup(popupContent, {
+  className: 'popup_wrapper', // ← ここで外側にクラスを追加
+  autoPan: true,
+  keepInView: true,
+})
+  .addTo(map);
+
+//  Dubai
+    var popupContent = `
+      <div class="popup_summer">
+    <a href="https://lh3.googleusercontent.com/pw/AP1GczMuyA6PtvVMNcsHFq5pBmUGW9BLRnIzrcVHprpIvRTyZDK3v28W9eCC4bdIGJ0Xh40CIce1YP4BoGizd8PCq5Ch5Vs4ETUuLwJcz-sPSebc8w6rMI_HhGCXdhq38qQWKDjafA11HVhSJcvLLGUkwg7xsg=w1216-h913-s-no-gm?authuser=0" 
+       data-lightbox="image" 
+       data-title="Dubai">
+      <img class="popup_img" loading="eager" src="https://lh3.googleusercontent.com/pw/AP1GczMuyA6PtvVMNcsHFq5pBmUGW9BLRnIzrcVHprpIvRTyZDK3v28W9eCC4bdIGJ0Xh40CIce1YP4BoGizd8PCq5Ch5Vs4ETUuLwJcz-sPSebc8w6rMI_HhGCXdhq38qQWKDjafA11HVhSJcvLLGUkwg7xsg=w1216-h913-s-no-gm?authuser=0" alt="Dubai Airport">
+    </a>
+    <div class="popup_caption">
+    <a href="https://lh3.googleusercontent.com/pw/AP1GczMuyA6PtvVMNcsHFq5pBmUGW9BLRnIzrcVHprpIvRTyZDK3v28W9eCC4bdIGJ0Xh40CIce1YP4BoGizd8PCq5Ch5Vs4ETUuLwJcz-sPSebc8w6rMI_HhGCXdhq38qQWKDjafA11HVhSJcvLLGUkwg7xsg=w1216-h913-s-no-gm?authuser=0" data-lightbox="image" data-title="Dubai Airport" class="popup_link">Dubai Airport</a>
+    <p>
+    Dubaiの説明
+    </p>
+    </div>
+    <a onclick="toggleSection('nextVisitSection')" class="popup_link">▼ Next Airport</a>
+    <div id="nextVisitSection" class="popup_section">
+    <div class="next_card" id="DubaiToHeathrowCard">
+    <p class="next_text">&#x2708; London（6000km）</span>
+    </div>
+    <div class="next_card" id="DubaiToGeneveCard">
+    <p class="next_text">&#x2708; Geneve（5500km）</span>
+    </div>
+    <div class="next_card" id="DubaiToViennaCard">
+    <p class="next_text">&#x2708; Vienna（5000km）</span>
+    </div>
+    </div>
+  </div>
+`;
+var markerDubai = L.marker([25.24916816381139, 55.361520925952654], {
   icon: AirportIcon 
 })
   .bindPopup(popupContent, {
